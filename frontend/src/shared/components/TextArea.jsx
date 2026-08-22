56d6d112706977a454dc9aa627bff80ea2a85caf
Ricardo Chaves Campos <richavesca@est.utn.ac.cr>
2026-08-21 18:23:13 -0600

    fix: Cambios dentro del textArea.
    - Cambios dentro del textArea para realizar que capture errores.
    - Cambios para mostrar diferentes estandares segun las necesidades como el asterisko.
    - Y utilizacion del componente Texto para escribir las cosas necesarias

--- frontend/src/shared/components/TextArea.jsx
+++ frontend/src/shared/components/TextArea.jsx
@@ -4,16 +4,37 @@
 
 //<TextArea
     //id="descripcion"
+    //name="descripcion"
     //label="Descripción"
     //placeholder="Ingrese una descripción"
+    //rows={5}
+    //required
 ///>
 
+import Texto from "./Texto";
 
-export default function TextArea({ id, label, placeholder, value, onChange, required = false }) {
+
+export default function TextArea({ id, label, placeholder, value, onChange, required = false, error, name, disabled = false, rows, className }) {
     return (
-        <div className="form-floating">
-            <textarea className="form-control" id={id} placeholder={placeholder} value={value} onChange={onChange} required={required} />
-            <label htmlFor={id}>{label}</label>
+        <div>
+            <Texto
+                texto={
+                    <>
+                        {label}
+                        {required && <span className="text-danger"> *</span>}
+                    </>
+                }
+                alineado="left"
+                color_text="black"
+            />
+            <textarea id={id} name={name} placeholder={placeholder} value={value} onChange={onChange} required={required} disabled={disabled} rows={rows} className={`form-control ${className || ""} ${error ? "is-invalid" : ""}`} />
+            {error && (
+                <Texto
+                    texto={error}
+                    alineado="left"
+                    color_text="red"
+                />
+            )}
         </div>
     )
 }
